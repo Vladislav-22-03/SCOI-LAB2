f09faa490c1f7bb76fd02e79c28695ef6d73360f
Pirojok_ot_Lovet <145265021+Vladislav-22-03@users.noreply.github.com>
2025-03-31 21:35:51 +0300

    2 lab

--- СЦОИ лаба 1/Form1.Designer.cs
+++ СЦОИ лаба 1/Form1.Designer.cs
@@ -17,88 +17,102 @@
 
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.histogramPictureBox = new System.Windows.Forms.PictureBox();
-            this.curvePictureBox = new System.Windows.Forms.PictureBox();
-            this.buttonResetCurve = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.histogramPictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.curvePictureBox)).BeginInit();
-            this.SuspendLayout();
-
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            histogramPictureBox = new PictureBox();
+            curvePictureBox = new PictureBox();
+            buttonResetCurve = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)histogramPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)curvePictureBox).BeginInit();
+            SuspendLayout();
+            // 
             // pictureBox1
-            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-
+            // 
+            pictureBox1.Location = new Point(10, 12);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(724, 663);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
             // button1
-            this.button1.Location = new System.Drawing.Point(820, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 60);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "📂 Открыть";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-
+            // 
+            button1.Location = new Point(784, 12);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(236, 75);
+            button1.TabIndex = 1;
+            button1.Text = "📂 Открыть";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // flowLayoutPanel1
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(820, 80);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 400);
-            this.flowLayoutPanel1.TabIndex = 2;
-            this.flowLayoutPanel1.WrapContents = false;
-
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(784, 100);
+            flowLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(236, 500);
+            flowLayoutPanel1.TabIndex = 2;
+            flowLayoutPanel1.WrapContents = false;
+            // 
             // histogramPictureBox
-            this.histogramPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.histogramPictureBox.Location = new System.Drawing.Point(10, 620);
-            this.histogramPictureBox.Name = "histogramPictureBox";
-            this.histogramPictureBox.Size = new System.Drawing.Size(300, 150);
-            this.histogramPictureBox.TabIndex = 3;
-            this.histogramPictureBox.TabStop = false;
-
+            // 
+            histogramPictureBox.BorderStyle = BorderStyle.FixedSingle;
+            histogramPictureBox.Location = new Point(10, 704);
+            histogramPictureBox.Margin = new Padding(3, 4, 3, 4);
+            histogramPictureBox.Name = "histogramPictureBox";
+            histogramPictureBox.Size = new Size(308, 262);
+            histogramPictureBox.TabIndex = 3;
+            histogramPictureBox.TabStop = false;
+            // 
             // curvePictureBox
-            this.curvePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.curvePictureBox.Location = new System.Drawing.Point(320, 620);
-            this.curvePictureBox.Name = "curvePictureBox";
-            this.curvePictureBox.Size = new System.Drawing.Size(300, 150);
-            this.curvePictureBox.TabIndex = 4;
-            this.curvePictureBox.TabStop = false;
-            this.curvePictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.CurvePictureBox_Paint);
-            this.curvePictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CurvePictureBox_MouseDown);
-            this.curvePictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CurvePictureBox_MouseMove);
-            this.curvePictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.CurvePictureBox_MouseUp);
-
+            // 
+            curvePictureBox.BorderStyle = BorderStyle.FixedSingle;
+            curvePictureBox.Location = new Point(324, 704);
+            curvePictureBox.Margin = new Padding(3, 4, 3, 4);
+            curvePictureBox.Name = "curvePictureBox";
+            curvePictureBox.Size = new Size(272, 262);
+            curvePictureBox.TabIndex = 4;
+            curvePictureBox.TabStop = false;
+            curvePictureBox.Paint += CurvePictureBox_Paint;
+            curvePictureBox.MouseDown += CurvePictureBox_MouseDown;
+            curvePictureBox.MouseMove += CurvePictureBox_MouseMove;
+            curvePictureBox.MouseUp += CurvePictureBox_MouseUp;
+            // 
             // buttonResetCurve
-            this.buttonResetCurve.Location = new System.Drawing.Point(630, 620);
-            this.buttonResetCurve.Name = "buttonResetCurve";
-            this.buttonResetCurve.Size = new System.Drawing.Size(120, 30);
-            this.buttonResetCurve.TabIndex = 5;
-            this.buttonResetCurve.Text = "Сбросить кривую";
-            this.buttonResetCurve.UseVisualStyleBackColor = true;
-            this.buttonResetCurve.Click += new System.EventHandler(this.buttonResetCurve_Click);
-
+            // 
+            buttonResetCurve.Location = new Point(703, 775);
+            buttonResetCurve.Margin = new Padding(3, 4, 3, 4);
+            buttonResetCurve.Name = "buttonResetCurve";
+            buttonResetCurve.Size = new Size(120, 38);
+            buttonResetCurve.TabIndex = 5;
+            buttonResetCurve.Text = "Сбросить кривую";
+            buttonResetCurve.UseVisualStyleBackColor = true;
+            buttonResetCurve.Click += buttonResetCurve_Click;
+            // 
             // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1032, 783);
-            this.Controls.Add(this.buttonResetCurve);
-            this.Controls.Add(this.curvePictureBox);
-            this.Controls.Add(this.histogramPictureBox);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Редактор изображений";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.histogramPictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.curvePictureBox)).EndInit();
-            this.ResumeLayout(false);
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1032, 979);
+            Controls.Add(buttonResetCurve);
+            Controls.Add(curvePictureBox);
+            Controls.Add(histogramPictureBox);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Редактор изображений";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)histogramPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)curvePictureBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
